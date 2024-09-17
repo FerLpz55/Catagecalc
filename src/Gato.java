ac5beb40e3665cbdbc076f7181acd33907bfef0a
FerLpz55 <rodriguezlopezfernando26@gmail.com>
2024-09-16 19:54:39 -0600

    first commit

---
+++ src/Gato.java
@@ -0,0 +1,21 @@
+public class Gato {
+
+    private int edad;
+    private String nombre;
+    private String raza;
+
+    /**
+     * @param edad
+     * @param nombre
+     * @param raza
+     */
+    public Gato (int edad, String nombre, String raza){
+        this.edad = edad;
+        this.nombre= nombre;
+        this.raza = raza;
+    }
+
+    public int calculare
+
+
+}
